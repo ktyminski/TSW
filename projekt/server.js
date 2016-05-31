136e6492c147a0d682d8845008d8503773004fd6
ktyminski <karoltyminskikarol@gmail.com>
2016-05-31 05:09:57 +0200

    Przygotowywanie aplikacji pod passporta do logowania

--- projekt/server.js
+++ projekt/server.js
@@ -11,6 +11,19 @@ var waterfall = require('async-waterfall');
 var port = process.env.PORT || 3000;
 var statics = require('serve-static');
 var io = require("socket.io")(httpServer);
+var express  = require('express');
+var app      = express();
+var port     = process.env.PORT || 8080;
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash    = require('connect-flash');
+
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
+
+var configDB = require('./config/database.js');
 
 
 var Horse = require('./models/horsemodel');
@@ -33,6 +46,20 @@ db.once('open', function () {
   console.log("connected to mongodb");
 });
 
+//------------------------------------PASSPORT----------------------------
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyParser());
+
+app.set('view engine', 'ejs');
+
+// required for passport
+app.use(session({ secret: 'ilovescotchscotchyscotchscotch' }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+//require('./app/routes.js')(app, passport);
 
 //-------------------------------------SOCKET------------------------------
 //-------------------------------------HORSE----------------------------------
